0e4081352ad368e2513503dd55233c06eadc3d31
Софья Григорович <sonyashkin@icloud.com>
2023-08-01 19:27:40 +0300

    some fix

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Label(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Имя')
+    name = models.CharField(max_length=100, verbose_name='Имя', unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
